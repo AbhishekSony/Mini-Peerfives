738996bb5068e49cb043d8debae005dd6e6f42b6
Abhishek Soni <abhisheksoni.coder@gmail.com>
2024-11-14 16:45:42 +0530

    Add models User and RewardHistory

---
+++ db/migrate/20241114111305_create_reward_histories.rb
@@ -0,0 +1,12 @@
+class CreateRewardHistories < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reward_histories do |t|
+      t.datetime :timestamp
+      t.integer :points
+      t.string :given_by
+      t.string :given_to
+
+      t.timestamps
+    end
+  end
+end
